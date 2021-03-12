dd51958a3d31dbdf74b249d560577d24f5ceb48d
Sage Fiorentino-Lange <sage.fio.lange@gmail.com>
2021-03-11 18:01:21 -0700

    now im testing the router switch thing again oh boy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 import DocUI from './components/DocUI'
-import Head from './components/Head';
+import { Layout, Menu, Button } from 'antd';
+import { PlusSquareOutlined } from '@ant-design/icons';
 
 import './App.global.css';
+import logo from '../assets/icon.svg';
+
+const { Header } = Layout;
 
 const openDocs = [
   { name: 'doc1', uri: '../assets/test1.pdf' },
@@ -11,45 +15,69 @@ const openDocs = [
   { name: 'doc3', uri: '../assets/test3.jpg' },
 ];
 
-interface IDocViewProps {
-  docIdx: number
+interface IHeadProps {
+  docNames: string[],
+  curDocIdx?: number,
 }
 
-class DocView extends React.Component<IDocViewProps> {
+class Head extends React.Component<IHeadProps> {
 
   render() {
     return (
-      <Head docNames={openDocs.map((doc) => doc.name)} curDocIdx={this.props.docIdx}>
-        <DocUI doc={openDocs[this.props.docIdx]} />
-      </Head>
-    )
-  }
-}
-
-class SearchView extends React.Component {
-  render() {
-    return (
-      <Head docNames={openDocs.map((doc) => doc.name)} curDocIdx={null}>
-        <div>
-          Hello!
-        </div>
-      </Head>
-    )
-  }
-}
-
+      <Layout style={{ height: '100vh' }}>
+        <Header
+          className="header"
+          style={{ background: '#fff1b8', padding: '0' }}
+        >
+          <Menu
+            theme='light'
+            mode='horizontal'
+            style={{ background: 'none' }}
+            selectedKeys={this.props.curDocIdx ? [`${this.props.curDocIdx}`] : []}
+          >
+            <img
+              src={logo}
+              alt="PAIGE Logo"
+              height="40px"
+              style={{ padding: '0 30px 0 43px', fill: '#002140' }}
+            />
+            <Link to='/'>Home</Link>
+            {this.props.docNames.map((name, idx) => {
+              return (
+                <Menu.Item key={idx}>
+                  <Link to={`/${name}`}>
+                    {name}
+                  </Link>
+                </Menu.Item>
+              );
+            })}
+            <Button
+              icon={<PlusSquareOutlined style={{ color: 'white' }} />}
+              size="large"
+              style={{ margin: '0 20px', background: '#002140' }}
+            />
+          </Menu>
+        </Header>
+        {this.props.children}
+      </Layout>
+    );
+  };
+};
 
 export default function App() {
   return (
     <HashRouter>
-      <Switch>
-        <Route path='/' component={SearchView} />
-        {openDocs.map((doc, idx) => {
-          return (
-            <Route path={`/${doc.name}`} render={() => <DocView docIdx={idx} /> }/>
-          )
-        })}
-      </Switch>
+      <Head docNames={openDocs.map((doc) => doc.name)}>
+        <Switch>
+          <Route path='/' render={() => <div>Hello!</div>}/>
+          {openDocs.map((docu) => {
+            return (
+              <><div>what up!</div>
+                <Route path={`/${docu.name}`} render={() => <DocUI doc={docu} />} /></>
+            )
+          })}
+        </Switch>
+      </Head>
     </HashRouter>
   )
 };
